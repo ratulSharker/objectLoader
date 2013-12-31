096e726be4dbd94a712e027686bd1784f9bc47e3
ratul <sharker.ratul.08@gmail.com>
2013-12-31 15:06:39 +0600

    this is a second commit

--- ObjParserLib/src/ObjParserLib.cpp
+++ ObjParserLib/src/ObjParserLib.cpp
@@ -3,7 +3,7 @@
 // Author      : ratul
 // Version     :
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : ObjectParser in C++, Ansi-style
 //============================================================================
 //============================================================================
 // Name        : OpenGlCube.cpp
